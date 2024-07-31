b064f84d982c88f7e548d61212e01783d9a4b97b
Desenvolvimento <dsadm@educ501.local>
2024-07-30 22:27:04 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import FooterBot from '@/components/FooterBot.vue'
+</script>
+
+<template>
+  <main id="card-app">
+    <div id="content-app">
+      <header>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/build">Build</RouterLink>
+            <RouterLink to="/cart">Cart</RouterLink>
+            <RouterLink to="/gofigoih">NotFound</RouterLink>
+        </nav>
+      </header>
+      <RouterView />
+    </div>
+    <footer class="flex flex-row align-items-end justify-content-end">
+      <div class="footer-bots flex flex-row justify-content-evenly">
+        <FooterBot img="@/assets/images/bot0.png" 
+          text="Crie seu robô com o máximo de tecnologia!" />
+        <FooterBot img="bot1.png" 
+          text="A plataforma mais amada do Brasil!" />
+        <FooterBot img="bot2.png" 
+          text="Atendimento online 24hrs!" />
+      </div>
+    </footer>      
+  </main>
+  
+
+  
+</template>
+
+<style scoped lang="scss">
+
+#card-app{
+  height: 90%;
+  width: 80%;
+  background-color: var(--app-card-background);
+  border-radius: 1.5rem;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+}
+
+footer {
+  width: 100%;
+  height: 20%;
+  max-height: 10rem;
+  background-color: var(--app-footer-background);
+  border-radius: 0 0 1.5rem 1.5rem;
+}
+</style>
