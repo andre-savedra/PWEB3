9814b2d1dbf151b493311438bffafdc64c873bc0
andre-savedra <andre_savedra@hotmail.com>
2024-08-01 22:24:54 -0300

    aula turma B

---
+++ turmaB/mange-bot/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>   
+  <main id="card-app">
+    <div id="content-app">
+      <header>
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/cart">Cart</RouterLink>
+          <RouterLink to="/build">Build</RouterLink>
+        </nav>
+      </header>
+
+      <RouterView /> 
+    </div>
+    <footer>
+
+    </footer>
+  </main>
+
+</template>
+
+
+<style scoped>
+
+</style>
